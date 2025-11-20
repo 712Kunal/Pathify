3e265347e43e51f500100a49f21cde6c771627a1
712Kunal <jadhavkun0@gmail.com>
2025-11-20 17:06:57 +0530

    added flow in the middleware

--- apps/server/middlewares/auth.middleware.js
+++ apps/server/middlewares/auth.middleware.js
@@ -2,8 +2,20 @@ import jwt from "jsonwebtoken";
 import logger from "../utils/logger.js";
 import ApiError from "../utils/ApiError.js";
 
+// FLOW -->
+// Login → accessToken returned in JSON → store in memory
+//        refreshToken stored in HttpOnly cookie
+
+// Requests → send accessToken in Authorization header
+
+// If accessToken expired → call /refresh → new accessToken
+
+// Logout → clear refreshToken cookie + remove stored accessToken
+
 export const authMiddleware = (req, res, next) => {
   const authHeader = req.headers.authorization;
+
+  // Authorization: Bearer <accessToken>
 
   if (!authHeader) {
     logger.error("No token provided");
